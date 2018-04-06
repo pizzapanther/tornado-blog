3ca54c3a3d7bbcb4fc66f392febab020285b05c2
Paul Bailey <paul.m.bailey@gmail.com>
2018-04-06 00:48:30 +0000

    more stuff

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import tornado.ioloop
 import tornado.web
 import tornado.log
 
+import markdown2
 import queries
 
 from jinja2 import \
@@ -36,11 +37,34 @@ class BlogPostHandler(TemplateHandler):
       {'slug': slug}
     )
     
-    self.render_template("post.html", {'post': posts[0]})
+    html = markdown2.markdown(posts[0]['body'])
+    context = {
+      'post': posts[0],
+      'html': html
+    }
+    self.render_template("post.html", context)
+    
+class CommentHandler(TemplateHandler):
+  def get (self, slug):
+    posts = self.session.query(
+      'SELECT * FROM post WHERE slug = %(slug)s',
+      {'slug': slug}
+    )
+    self.render_template("comment.html", {'post': posts[0]})
+    
+  def post (self, slug):
+    comment = self.get_body_argument('comment')
+    posts = self.session.query(
+      'SELECT * FROM post WHERE slug = %(slug)s',
+      {'slug': slug}
+    )
+    # Save Comment Here
+    self.redirect('/post/' + slug)
     
 def make_app():
   return tornado.web.Application([
     (r"/", MainHandler),
+    (r"/post/(.*)/comment", CommentHandler),
     (r"/post/(.*)", BlogPostHandler),
     (r"/static/(.*)", 
       tornado.web.StaticFileHandler, {'path': 'static'}),
